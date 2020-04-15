7c05f44389488f14824e5fd7f8ea7999dd446bf5
Семен <43268369+PrincePepper@users.noreply.github.com>
2020-04-15 22:06:06 +1000

    alfa

---
+++ app/src/main/java/mesto/ridom/mestoridom/IntroActivity.java
@@ -0,0 +1,71 @@
+package mesto.ridom.mestoridom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    int a = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        final TextView textViewGip = findViewById(R.id.intro_giptext);
+        final TextView textView = findViewById(R.id.intro_text);
+        final TextView textViewH = findViewById(R.id.intro_htext);
+        final ImageView imageView = findViewById(R.id.intro_image);
+        final Button button = findViewById(R.id.intro_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (a) {
+                    case 0:
+                        textView.setText(R.string.intro_text_2);
+                        textViewH.setText(R.string.intro_htext_2);
+                        imageView.setImageResource(R.drawable.ic_e_loupe);
+                        a++;
+                        break;
+                    case 1:
+                        textView.setText(R.string.intro_text_3);
+                        textViewH.setText(R.string.intro_htext_3);
+                        imageView.setImageResource(R.drawable.ic_e_stars);
+                        a++;
+                        break;
+                    case 2:
+                        textView.setText(R.string.intro_text_4);
+                        textViewH.setText(R.string.intro_htext_4);
+                        imageView.setImageResource(R.drawable.ic_e_abc);
+                        a++;
+                        break;
+                    case 3:
+                        textView.setText(R.string.intro_text_5);
+                        textViewH.setText(R.string.intro_htext_5);
+                        imageView.setImageResource(R.drawable.mesto_ridom);
+                        a++;
+                        break;
+                    case 4:
+                        Intent SplashIntent = new Intent(IntroActivity.this, NavigationActivity.class);
+                        IntroActivity.this.startActivity(SplashIntent);
+                        IntroActivity.this.finish();
+                }
+            }
+        });
+        textViewGip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent SplashIntent = new Intent(IntroActivity.this, NavigationActivity.class);
+                IntroActivity.this.startActivity(SplashIntent);
+
+                IntroActivity.this.finish();
+            }
+        });
+    }
+}
--- app/src/main/java/mesto/ridom/mestoridom/SplashScreenActivity.java
+++ app/src/main/java/mesto/ridom/mestoridom/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package mesto.ridom.mestoridom;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -10,22 +12,49 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private static final String MY_SETTINGS = "my_settings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splach_screen);
+        SharedPreferences sp = getSharedPreferences(MY_SETTINGS,
+                Context.MODE_PRIVATE);
+        // проверяем, первый ли раз открывается программа
+        boolean hasVisited = sp.getBoolean("hasVisited", false);
+        if (!hasVisited) {
+            // выводим нужную активность
 
-        startSplashAmimation();
+            SharedPreferences.Editor e = sp.edit();
+            e.putBoolean("hasVisited", true);
+            e.apply(); // не забудьте подтвердить изменения
+            startSplashAmimation();
+        }else{
+            startSplashAmimation2();
+        }
+
     }
-
-    private void startSplashAmimation() {
+    private void startSplashAmimation2() {
         startAnimation();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
                 Intent SplashIntent = new Intent(SplashScreenActivity.this, NavigationActivity.class);
+                SplashScreenActivity.this.startActivity(SplashIntent);
+
+                SplashScreenActivity.this.finish();
+
+            }
+        }, 3000);
+    }
+    private void startSplashAmimation() {
+        startAnimation();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent SplashIntent = new Intent(SplashScreenActivity.this, IntroActivity.class);
                 SplashScreenActivity.this.startActivity(SplashIntent);
 
                 SplashScreenActivity.this.finish();
