2008adc75285a55d5a0a14e10b6cfaceca450bc6
PrincePepper <semyon3336@gmail.com>
2021-02-18 14:23:09 +1000

    # upgrade func hideKeyboard

--- app/src/main/java/mesto/ridom/mestoridom/activities/BaseActivity.java
+++ app/src/main/java/mesto/ridom/mestoridom/activities/BaseActivity.java
@@ -61,7 +61,7 @@ public class BaseActivity extends AppCompatActivity {
 
     }
 
-    protected void hideKeyboard(Activity activity) {
+    protected static void hideKeyboard(Activity activity) {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         //Find the currently focused view, so we can grab the correct window token from it.
         View view = activity.getCurrentFocus();
